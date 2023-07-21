df26233ccd3061f94f648bba103dbeffe2e08e5d
MaximeCOMTE <97729276+MaximeCOMTE@users.noreply.github.com>
2023-07-21 09:22:28 +0200

    9h20

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 var express = require('express');
 var app = express();
+const bodyParser = require('body-parser');
 
 // '/' est la route racine
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', function (req, res) {
-  res.send('Bonjour !');
+  res.send(`
+        <form action="/login" method="post">
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username">
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password">
+            <button type="submit">Login</button>
+        </form>
+    `);
 });
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  // Here you should implement your logic to check the username and password
+  // If they are valid, you can set the user session and redirect the user to their dashboard
+  // If they are not valid, you can redirect them back to the login page with an error message
+  res.send(`Username: ${username}, Password: ${password}`);
+});
+
 
 app.listen(4000, function () {
   console.log("Application d'exemple écoutant sur le port 4000 !");
